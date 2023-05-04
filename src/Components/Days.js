1ffbe8722a835a90b600304ce8f0c10c502373d4
Julien-Martin-g404 <julienmartin2802@gmail.com>
2023-05-04 11:54:47 +0200

    new forlder

--- src/Components/Days.js
+++ src/Components/Days.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import '../Styles/Days.css'
+import React from 'react';
+import '../Styles/Days.css';
 
-function Days() {
+function Days(props) {
+    const daysOfWeek = ['Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+    const handleDayClick = (index) => {
+        props.handleDayClick(index);
+    };
+
     return (
-
-        <div class="card-action">
-            <a href="#" className="font" >Thursday</a>
-            <a href="#" className="font">Friday</a>
-            <a href="#" className="font">Saturday</a>
-            <a href="#" className="font">Sunday</a>
-            <a href="#" className="font">Monday</a>
+        <div className="card-action">
+            {daysOfWeek.map((day, index) => (
+                <a key={index} href="#" className={props.activeDay === index ? 'font active' : 'font'} onClick={() => handleDayClick(index)}>
+                    {day}
+                </a>
+            ))}
         </div>
-
-    )
+    );
 }
 
-export default Days
+export default Days;
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,57 +1,91 @@
-import Sun from '../Assets/sun.svg';
+import React, { useState, useEffect } from 'react';
 import '../Styles/Weather.css';
 import Days from './Days.js';
-import React, { useState, useEffect } from 'react';
 
 function Weather() {
-
     const [weatherData, setWeatherData] = useState(null);
+    const [activeDay, setActiveDay] = useState(0);
+    const [city, setCity] = useState(''); // Valeur initiale vide pour la ville
 
     useEffect(() => {
         const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-        const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=lyon&days=5&aqi=no&alerts=no`;
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => setWeatherData(data))
-    }, []);
+        let apiUrl;
+
+        if (city === '') { // Si la ville est vide, on utilise la géolocalisation
+            navigator.geolocation.getCurrentPosition((position) => {
+                apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${position.coords.latitude},${position.coords.longitude}&days=5&aqi=no&alerts=no`;
+                fetch(apiUrl)
+                    .then((response) => response.json())
+                    .then((data) => setWeatherData(data))
+                    .catch((error) => console.error(`Error fetching weather data: ${error}`));
+            });
+        } else { // Sinon, on utilise la ville saisie par l'utilisateur
+            apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5&aqi=no&alerts=no`;
+            fetch(apiUrl)
+                .then((response) => response.json())
+                .then((data) => setWeatherData(data))
+                .catch((error) => console.error(`Error fetching weather data: ${error}`));
+        }
+    }, [city]);
+
+    const handleDayClick = (index) => {
+        setActiveDay(index);
+    };
+
+    const currentDay = new Date().getDay();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newCity = e.target.elements.city.value;
+        setCity(newCity);
+    };
 
     return (
-
         <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="city">Enter city name :</label>
+                <input type="text" id="city" name="city" defaultValue={city} />
+                <button type="submit">Submit</button>
+            </form>
             {weatherData ? (
-                <div class="weather card blue-grey darken-1">
-                    <div class="card-content white-text">
-                        <div class="card-title">{weatherData.location.name}</div>
-                        <img src={Sun} class="App-logo" alt="logo2" />
-                        <div class="temperature">{weatherData.current.temp_c} °C</div>
-                        <div class="wind">Vent:  {weatherData.current.wind_kph} Km/h</div>
+                <div className="weather card blue-grey darken-1">
+                    <div className="card-content white-text">
+                        <div className="card-title">{weatherData.location.name}</div>
+                        <img
+                            src={weatherData.current.condition.icon}
+                            className="App-logo"
+                            alt="logo2"
+                        />
+                        <div className="temperature">
+                            {activeDay === 0
+                                ? weatherData.current.temp_c
+                                : weatherData.forecast.forecastday[activeDay].day.avgtemp_c}{' '}
+                            °C
+                        </div>
+                        <div className="wind">
+                            Vent:{' '}
+                            {activeDay === 0
+                                ? weatherData.current.wind_kph
+                                : weatherData.forecast.forecastday[activeDay].day.maxwind_kph}{' '}
+                            Km/h (
+                            {activeDay === 0
+                                ? weatherData.current.wind_degree
+                                : weatherData.forecast.forecastday[activeDay].day.wind_degree}
+                            °)
+                        </div>
                     </div>
-
-                    <Days />
+                    <Days
+                        handleDayClick={handleDayClick}
+                        currentDay={currentDay}
+                        activeDay={activeDay}
+                    />
                 </div>
             ) : (
                 <p>Loading...</p>
             )}
         </div>
-
-
-        // <div class="weather card blue-grey darken-1">
-        //     <div class="card-content white-text">
-        //         <div class="card-title">{weatherData.location.name}</div>
-        //         <img src={Sun} class="App-logo" alt="logo2" />
-        //         <div class="temperature">{weatherData.current.temp_c}</div>
-        //         <div class="wind">{weatherData.current.wind_kph}</div>
-        //     </div>
-
-        //     <Days />
-        // </div>
-
-
-
-
-
-
     );
+
 }
 
-export default Weather
+export default Weather;
